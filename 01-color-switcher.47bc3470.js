8e23249bbe4be5218e6327c7d140aa3cd684c06f
AlinaKalen <134622659+AlinaKalen@users.noreply.github.com>
2023-10-09 20:48:24 +0000

    Deploying to gh-pages from @ AlinaKalen/goit-js-hw-09@5c7626fbdbd662e3be2e2902a5a4bbc976ed5568 🚀

---
+++ 01-color-switcher.47bc3470.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(()=>{null===d&&(d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3),e.disabled=!0,t.disabled=!1)})),t.addEventListener("click",(()=>{clearInterval(d),d=null,e.disabled=!1,t.disabled=!0}))}));
+//# sourceMappingURL=01-color-switcher.47bc3470.js.map
