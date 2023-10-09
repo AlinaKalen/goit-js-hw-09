5c7626fbdbd662e3be2e2902a5a4bbc976ed5568
AlinaKalen <alina103599@gmail.com>
2023-10-09 21:47:55 +0100

    task-1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+  
+    let intervalId = null;
+  
+    startButton.addEventListener('click', () => {
+      if (intervalId === null) {
+        intervalId = setInterval(() => {
+          document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+  
+        startButton.disabled = true;
+        stopButton.disabled = false;
+      }
+    });
+  
+    stopButton.addEventListener('click', () => {
+      clearInterval(intervalId);
+      intervalId = null;
+      startButton.disabled = false;
+      stopButton.disabled = true;
+    });
+  });
