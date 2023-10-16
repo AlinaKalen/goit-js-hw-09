9931f9c197f78a71914c157b20c94fbeefb60d99
AlinaKalen <alina103599@gmail.com>
2023-10-16 14:54:40 +0100

    task  - 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,7 +18,7 @@ const options = {
       startButton.disabled = true;
     } else {
       startButton.removeAttribute("disabled");
-      targetDate = selectedDate; // Set the targetDate when a valid date is selected
+      targetDate = selectedDate; 
     }
   }
 };
@@ -47,26 +47,28 @@ function updateTimer() {
     const now = new Date();
     const timeRemaining = targetDate - now;
     let date = convertMs(timeRemaining);
-    let formattedTime = date.days + " Days" + date.hours + " Hours" + date.minutes + " Minutes" + date.seconds + " Seconds";
+    let formattedTime = `${date.days} Days\n${date.hours} Hours\n${date.minutes} Minutes\n${date.seconds} Seconds`;
     timerElement.textContent = formattedTime; 
 }
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
   
-    // Remaining days
+
     const days = Math.floor(ms / day);
-    // Remaining hours
+   
     const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
+    
     const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
+   
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
+  }
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
   }
   
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const firstDelay = parseInt(document.querySelector('input[name="delay"]').value);
+  const step = parseInt(document.querySelector('input[name="step"]').value);
+  const amount = parseInt(document.querySelector('input[name="amount"]').value);
+
+  if (isNaN(firstDelay) || isNaN(step) || isNaN(amount)) {
+    alert('Please enter valid values for delay, step, and amount.');
+    return;
+  }
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const delay = firstDelay + i * step;
+
+    createPromise(position, delay)
+          .then(({ position, delay }) => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+            Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+      }
+    });
